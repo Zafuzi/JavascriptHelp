7e1653278ee34a38bc7b21d2e37baf2dc0c45e5d
Zach <zacharyfoutz@gmail.com>
2018-04-29 17:25:32 -0700

    Moved cloner function to pass back html elements after cloning, moved sensors to a modal

--- js/script.js
+++ js/script.js
@@ -1,20 +1,75 @@
 
-
-
 document.addEventListener("DOMContentLoaded", function(){
   doFirst();
+  addListeners();
   setData();
-  var provinces = [];
-  const url = './provinces.json';
-  if(provinces.length < 1){
-    // Populate dropdown with list of provinces
-    $.getJSON(url, function (data) {
-      //console.log(data);
-      provinces = data;
-      cloner("tpl_select_province", provinces, () => {}, () => {console.log(provinces)})
-    });
-  }
 })
+
+function addListeners(){
+  $('#unit_search').on('change', function(e){
+    let val = $(this).val();
+    let self = this;
+
+    let unit = UNITS.filter(u => {
+      return (u.SerialNumber == val);
+    });
+
+    console.log("UNIT: ", unit);
+
+    
+    cloner('tpl_table_row', unit, null, (s) => {
+      console.log("S: ", s);
+      if(s.length < 1) return;
+      $(".main-table tbody").append(s);
+      $(".main-table tbody").removeClass('hid');
+      let option = $('option[value="' + val + '"]');
+      option.remove();
+      $(self).val('');
+    });
+  })
+
+  $('#myModal').on('shown.bs.modal', function(e) {
+      let val = $(e.relatedTarget).data('serial');
+      $('.modal-title').text("UNIT SN #" + val);
+      let unit = UNITS.filter(u => {
+        return (u.SerialNumber == val);
+      })[0];
+
+      console.log(unit);
+
+      $.each(unit.SENSORS, (key, sensor) => {
+        sensor.status_color = sensor.STATUS == "ON" ? "#6230D1" : "red";
+
+        if(sensor.POWER >= 75){
+          sensor.RISK = "LOW";
+        }else if (sensor.POWER >= 35){
+          sensor.RISK = "MEDIUM";
+        } else {sensor.RISK = "HIGH"}
+
+        switch(sensor.RISK){
+            case "HIGH":
+              sensor.risk_color = "red";
+              break;
+            case "MEDIUM":
+              sensor.risk_color = "yellow";
+              break;
+            case "LOW":
+              sensor.risk_color = "green";
+              break;
+        }
+      });
+      console.log("SENSORS: ", unit.SENSORS);
+      $(".sensor-table tbody").html('');
+      cloner('tpl_sensor_row', unit.SENSORS, null, (s) => {
+        console.log("S: ", s);
+        if(s.length < 1) return;
+        $(".sensor-table tbody").append(s);
+        $(".sensor-table tbody").removeClass('hid');
+      });
+  })
+}
+
+var UNITS = [];
 
 function setData(){
   var xhr = new XMLHttpRequest();
@@ -24,27 +79,49 @@ function setData(){
 
   xhr.onload = function (){
     if (xhr.status===200){
-      var myStuff = JSON.parse(xhr.responseText);
-      myStuff = myStuff.UNIT;
+      UNITS = JSON.parse(xhr.responseText).UNIT;
 
       var table = $('.main-table tbody');
       
+      $.each(UNITS, (key, UNIT) => {
+        UNIT.status_color = UNIT.STATUS == "ON" ? "#6230D1" : "red";
 
-      cloner('tpl_table_row', myStuff, 
-        function(){
-        }, () => { table.removeClass('hid'); }
-      );
+        if(UNIT.POWER >= 75){
+          UNIT.RISK = "LOW";
+        }else if (UNIT.POWER >= 35){
+          UNIT.RISK = "MEDIUM";
+        } else {UNIT.RISK = "HIGH"}
+
+        console.log(UNIT.POWER, UNIT.RISK);
+
+        switch(UNIT.RISK){
+            case "HIGH":
+              UNIT.risk_color = "red";
+              break;
+            case "MEDIUM":
+              UNIT.risk_color = "yellow";
+              break;
+            case "LOW":
+              UNIT.risk_color = "green";
+              break;
+        }
+      })
+
+      // cloner('tpl_table_row', UNITS, 
+      //   function(){
+      //   }, () => { table.removeClass('hid'); }
+      // );
+
+      cloner('tpl_unit_searial_datalist', UNITS, null, function(s){
+        $('#unit_serials').append(s);
+      })
     }
   }
 }
 
 function cloner(id, data, lp, cb){
+  console.log("DATA: ", data);
   try{
-    var elements = $('#' + id) || $('.' + id);
-    if(elements.length > 1){
-      console.log(elements);
-    }
-    console.log(elements);
     var el = $('#' + id)
     var top = el;
     var template = $(el)[0].outerHTML;
@@ -54,23 +131,60 @@ function cloner(id, data, lp, cb){
     $.each(data, (key, row) => {
       var s = template;
       $.each(row, (prop, text) => {
-        var regex = new RegExp('\\${' + prop + '}', "g");
+        var regex = new RegExp('\__' + prop + '__', "g");
         s = s.replace(regex, text);
+        s = s.replace(/\w*hid\b/g, '');
       })
-      
-      el.before($(s));
+      el_arr.push($(s));
       if(lp) lp();
     });
 
-    top.remove();
-    if(cb) cb(el_arr);
+    cb(el_arr);
   } catch (e){
     console.error("Error during cloning " + id + " error: "  + e );
   }
-  
 }
 
 function doFirst(){
+
+  // Make the first chart
+  var ctx = document.getElementById("chart0").getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+          labels: ["High Risk", "Medium Risk", "Low Risk", "Offline", "Online"],
+          datasets: [{
+              label: '# of UNITS',
+              data: [10, 16, 34, 10, 50],
+              backgroundColor: [
+                  'rgba(255, 166, 77, .2)',
+                  'rgba(255, 206, 86, .2)',
+                  'rgba(54, 162, 235, .2)',
+                  'rgba(175, 175, 175, .2)',
+                  'rgba(102, 0, 255,.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 166, 77, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(175, 175, 175, 1)',
+                  'rgba(102, 0, 255, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              xAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+
+
   $('.hover').click(function () {
     $('.table-head').toggle();
     $('.hide-main').toggle();
